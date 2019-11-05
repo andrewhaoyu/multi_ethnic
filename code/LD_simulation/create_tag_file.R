8a800030df192cebfb35649b181aa9377eb5cde5
Zhang <andrew.haoyu@gmail.com>
2019-11-04 21:50:56 -0500

    simulate_LD_data

--- code/LD_simulation/create_tag_file.R
+++ code/LD_simulation/create_tag_file.R
@@ -3,21 +3,21 @@
 #tag files are created seperately for each chr
 library(data.table)
 library(dplyr)
+eth <- c("AFR","AMR","EAS","EUR","SAS")
 for(i in 1:22){
- 
   leg <- as.data.frame(fread(paste0("/data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",i,".legend"),header=T))
+  TYPE = leg$TYPE
+  MAF <- leg[,c(6:10)]
+  for(k in 1:length(eth)){
+    idx <- which(MAF[,k]>=0.01&
+                   MAF[,k]<=0.99&
+                   TYPE=="Biallelic_SNP")
+    leg_chr = leg$position[idx] 
+    
+    
+    write.table(leg_chr,file = paste0("/data/zhangh24/KG.impute2/tag/",eth[k],"_chr",i,".tag"),row.names = F,col.names = F,quote=F)
+    
+  }
   
-  
-  leg_chr = leg %>% 
-    filter(AMR>=0.01&
-             EAS>=0.01&
-             EUR>=0.01&
-             SAS>=0.01&
-             AFR>=0.01&TYPE=="Biallelic_SNP") %>% 
-    select(position)
-  
-
-  write.table(leg_chr,file = paste0("/data/zhangh24/KG.impute2/tag/chr",i,".tag"),row.names = F,col.names = F,quote=F)
   
 }
-
--- code/LD_simulation/simulate_LD_data.R
+++ code/LD_simulation/simulate_LD_data.R
@@ -6,16 +6,27 @@
 
 library(data.table)
 
-eth <- c("EUR","AFR","AMR","EAS","SAS")
+eth <- c("AFR","AMR","EAS","EUR","SAS")
 
 code <- rep("c",length(eth)*10000)
 temp <- 1
 n <- c(120,180,180,180,180)
+tag_all <- list()
+for(i in 1:length(eth)){
   for(j in 1:22){
-    tag <- read.table(paste0("/spin1/users/zhangh24/KG.impute2/tag/chr",j,".tag"),header=F)
-  for(k in 1:100){
-    for(i in 1:length(eth)){
-      code[temp] <- paste0("/data/zhangh24/software/hapgen2 -m /spin1/users/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -t /spin1/users/zhangh24/KG.impute2/tag/chr",j,".tag -no_haps_output"
+    tag_all[[i]][[j]] <- read.table(paste0("/data/zhangh24/KG.impute2/tag/",eth[i],"_chr",j,".tag"),header=F)
+  }
+  
+}
+
+#for put chr and ethnic groups in inner loop to aviod the same start time for hapgen2
+#hapgen2 use the start time to set random seed
+for(k in 1:100){
+for(j in 1:22){
+    
+    for(i in 1:length(eth)){    
+      tag <- tag_all[[i]][[j]]
+      code[temp] <- paste0("/data/zhangh24/software/hapgen2 -m /spin1/users/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -t /data/zhangh24/KG.impute2/tag/",eth[i],"_chr",j,".tag -no_haps_output"
       )  
       temp = temp+1   
        }
@@ -31,14 +42,21 @@ code <- code[1:(temp-1)]
 write.table(code,file = paste0("/data/zhangh24/multi_ethnic/code/LD_simulation/simulate_LD_data.sh"),row.names = F,col.names = F,quote=F)
 
 
+
+
+
+
+
+
 code <- rep("c",length(eth)*10000)
 temp <- 1
 n <- c(120,180,180,180,180)
+
 for(j in 22){
   tag <- read.table(paste0("/spin1/users/zhangh24/KG.impute2/tag/chr",j,".tag"),header=F)
   for(k in 1:100){
     for(i in 1){
-      code[temp] <- paste0("/data/zhangh24/software/hapgen2 -m /spin1/users/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -t /spin1/users/zhangh24/KG.impute2/tag/chr",j,".tag -no_haps_output"
+      code[temp] <- paste0("TZ='EDT+",format(Sys.time()+temp*10,"%H:%M:%S"),"' /data/zhangh24/software/hapgen2 -m /spin1/users/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -t /spin1/users/zhangh24/KG.impute2/tag/chr",j,".tag -no_haps_output"
       )  
       temp = temp+1   
     }
