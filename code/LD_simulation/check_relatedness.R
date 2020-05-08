1e84a8f70a5a5cc8786ca76dd0d77cfd89e5f16a
Zhang <andrewhaoyu>
2020-05-08 15:17:02 -0400

    check relateness

--- code/LD_simulation/check_relatedness.R
+++ code/LD_simulation/check_relatedness.R
@@ -19,10 +19,17 @@ for(k in (1+(i1-1)*step):(i1*step)){
   print(k)
   result[k] <- max(cor(genotype_standard_train[,k],genotype_standard_test))  
 }
-save(result,file = paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/relatedness.rdata"))
+save(result,file = paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/relatedness_",i1,".rdata"))
 
 
-
+#merge the result
+# result_all <- rep(0,100000)
+# total <- 0
+# for(k in 1:1000){
+#   load(paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/relatedness.rdata"))
+#   temp <- length(result)
+#   result_all[total+(1:temp)] <- result
+# }
 
 #temp <- apply(genotype_standard,1,sd)
 #temp2 <- apply(genotype_standard,1,mean)
