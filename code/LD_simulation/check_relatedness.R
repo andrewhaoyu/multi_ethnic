a3f55d6ff5a56312015beae355af53747bdd5552
Zhang <andrewhaoyu>
2020-05-10 11:38:16 -0400

    check_relatedness

--- code/LD_simulation/check_relatedness.R
+++ code/LD_simulation/check_relatedness.R
@@ -11,14 +11,14 @@ for(i in 1:length(MAF)){
 genotype_standard_train = genotype_standard[,1:100000]
 genotype_standard_test = genotype_standard[,100001:110000]
 
-step = 100000/100
+step = 10000/100
 
 
 result <- rep(0,step)
 
 for(k in 1:step){
   print(k)
-  result[k] <- max(cor(genotype_standard_train[,k+(i1-1)*step],genotype_standard_test))  
+  result[k] <- max(cor(genotype_standard_train,genotype_standard_test[,k+(i1-1)*step]))  
 }
 save(result,file = paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/relatedness_",i1,".rdata"))
 
@@ -26,14 +26,18 @@ save(result,file = paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/
 #merge the result
 # result_all <- rep(0,100000)
 # total <- 0
-# for(i1 in 1:1000){
+# for(i1 in 1:100){
 #   load(paste0("/data/zhangh24/multi_ethnic/result/LD_simulation/EUR/relatedness_",i1,".rdata"))
 #   temp <- length(result)
 #   result_all[total+(1:temp)] <- result
+#   total <- total+temp
 # }
-
-#temp <- apply(genotype_standard,1,sd)
-#temp2 <- apply(genotype_standard,1,mean)
-
-a <- matrix(rnorm(6),3,2)
-cor(a)
+# 
+# idx <- which(result_all>=0.1)
+# 
+# 
+# #temp <- apply(genotype_standard,1,sd)
+# #temp2 <- apply(genotype_standard,1,mean)
+# 
+# a <- matrix(rnorm(6),3,2)
+# cor(a)
