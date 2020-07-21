0a690cf9f3584008f7fb4346c9fa2f5cb6c680e3
Zhang <andrewhaoyu>
2020-07-21 14:02:10 -0400

    simuate_LD_data_subset

--- code/LD_simulation/simulate_LD_data_subset.R
+++ code/LD_simulation/simulate_LD_data_subset.R
@@ -19,7 +19,7 @@ n <- c(120,120,120,120)
 
 tag<- read.table(paste0("/data/zhangh24/KG.impute2/tag/",eth[i],"_chr",j,".tag"),header=F)
 
-system(paste0("/data/zhangh24/software/hapgen2 -m /data/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /lstratch/",sid,"/test/",eth[i],"_chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -no_haps_output"))  
+system(paste0("/data/zhangh24/software/hapgen2 -m /data/zhangh24/KG.impute2/1000GP_Phase3/genetic_map_chr",j,"_combined_b37.txt -l /data/zhangh24/KG.impute2/1000GP_Phase3/1000GP_Phase3_chr",j,".legend -h /data/zhangh24/KG.impute2/",eth[i],"/chr",j,".hap -o /lscratch/",sid,"/test/",eth[i],"_chr",j,"_",k," -n ",n[i]," 1 -dl ",tag[1,1]," 1 1 1 -no_haps_output"))  
   
 
 #hapgen2 simulated the genotype data for all the SNPs
@@ -34,7 +34,7 @@ library(data.table)
 eth <- c("EUR","AFR","AMR","EAS")
 
 
-gen <- as.data.frame(fread(paste0("/lstratch/",sid,"/test/",eth[i],"_chr",j,"_",k,".controls.gen"),header=F))
+gen <- as.data.frame(fread(paste0("/lscratch/",sid,"/test/",eth[i],"_chr",j,"_",k,".controls.gen"),header=F))
 colnames(tag) <- "position"
 colnames(gen)[3] <- "position"
 tag.gen <- left_join(tag,gen,by="position")
