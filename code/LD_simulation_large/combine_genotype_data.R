23fbd924dfe76a48a722d31b86e3053ebc38feb2
Zhang <andrew.haoyu@gmail.com>
2020-07-28 14:03:45 -0400

    combine_genotype_data

---
+++ code/LD_simulation_large/combine_genotype_data.R
@@ -0,0 +1,22 @@
+#combine the by chromosome data into one file
+args <- commandArgs(trailingOnly = T)
+i <- as.numeric(args[[1]])
+n <- 22
+eth <- c("EUR","AFR","AMR","EAS","SAS")
+merge_list = rep("c",21)
+temp = 1
+for(j in 2:n){
+  merge_list[temp] <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_new/",eth[i],"/chr",j,".tag")
+  temp = temp+1
+}
+write.table(merge_list,file = paste0("/lscratch/",sid,"/test/",eth[i],"_all_files"),
+            col.names = F,
+            row.names = F,
+            quote=F)
+#merge all the seperate sample into one
+#merge the seperate genotype data into one file
+system(paste0("/data/zhangh24/software/plink2 --bfile /data/zhangh24/multi_ethnic/result/LD_simulation_new/",eth[i],"/chr",1,".tag --merge-list /lscratch/",sid,"/test/",eth[i],"_all_files", " --make-bed --out /data/zhangh24/multi_ethnic/result/LD_simulation_new/",eth[i],"/all_chr",".tag"))
+
+
+
+
