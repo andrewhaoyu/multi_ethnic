f82a4756b9185bd755f2ac1a81b79735402c5b55
Zhang <andrew.haoyu@gmail.com>
2020-07-29 22:23:19 -0400

    run_gwas

---
+++ code/LD_simulation_large/run_gwas.R
@@ -0,0 +1,15 @@
+#run GWAS using plink2
+#use plink2 to run
+args  = commandArgs(trailingOnly = T)
+#i represent ethnic group
+#j represent chromosome
+#l represent the causal SNPs proportion
+i = as.numeric(args[[1]])
+j = as.numeric(args[[2]])
+l = as.numeric(args[[3]])
+#load the phenotpypes data and use plink to run
+eth <- c("EUR","AFR","AMR","EAS")
+library(data.table)
+cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
+
+system(paste0("/data/zhangh24/software/plink2 --bfile ",cur.dir,eth[i],"/chr",j,".tag --out ",cur.dir,eth[i],"/summary_chr",j,"_rho_",l,".out --linear --all-pheno --allow-no-sex --pheno ",cur.dir,eth[i],"/pheno_plink_rho",l))
