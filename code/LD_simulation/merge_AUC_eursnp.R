7b2c6a62702b05148f92bf852a286af6c19e3a07
Zhang <andrew.haoyu@gmail.com>
2020-08-12 10:02:25 -0400

    update

---
+++ code/LD_simulation/merge_AUC_eursnp.R
@@ -0,0 +1,32 @@
+#merge the r2 results of AUC
+eth <- c("EUR","AFR","AMR","EAS","SAS")
+pthres <- c(5E-08,1E-07,5E-07,1E-06,5E-06,1E-05,5E-05,1E-04,1E-03,1E-02,1E-01)
+
+total <- 4*3*4
+cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
+eth.vec <- rep(0,total)
+r2.vec <- rep(0,total)
+l_vec <- rep(0,total)
+m_vec <- rep(0,total)
+temp = 1
+method <- c("eurcoef","tarcoef","eb")
+method_vec <- rep("c",total)
+for(q in 1:1){
+  for(i in 2:5){
+    for(l in 1:3){
+      for(m in 1:4){
+        load(paste0(cur.dir,eth[i],"/r2_eursnp_",method[q],"_rho_",l,"_size_",m))
+        eth.vec[temp] = eth[i]
+        r2.vec[temp] <- r2
+        l_vec[temp] <- l
+        m_vec[temp] <- m
+        method_vec[temp] <- method[q]
+        temp = temp+1
+      }
+    }
+  }
+  
+}
+#best r2 result by varying the p-value threshold
+LD.clump.result <- data.frame(eth.vec,r2.vec,l_vec,m_vec,method_vec)
+save(LD.clump.result,file = paste0(cur.dir,"best_eur_snp_result.rdata"))
--- code/LD_simulation_large/generate_plot.R
+++ code/LD_simulation_large/generate_plot.R
@@ -1,6 +1,7 @@
 setwd("/Users/zhangh24/GoogleDrive/multi_ethnic/result/LD_simulation_new")
 library(ggplot2)
 library(ggsci)
+#standard LD clumping results
 load("LD.clump.result.rdata")
 LD.clump.result <- LD.result.list[[1]]
 sample_size <- factor(rep(c("15000","45000","80000","100000"),15),
@@ -69,3 +70,46 @@ LD.clump.result <- cbind(LD.clump.result,sample_size,cau_vec)
       width = 15, height = 10, res = 300,units = "in")
   p
   dev.off()
+
+  
+#Best EUR result
+  load("LD.clump.result.rdata")
+  LD.clump.result <- LD.result.list[[1]]
+  sample_size <- factor(rep(c("15000","45000","80000","100000"),4*3*2),
+                        levels=c("15000","45000","80000","100000"))
+  
+  cau_vec <- as.character(LD.clump.result$l_vec)
+  csp <- c(0.01,0.001,0.0005)
+  LD.clump.result <- LD.clump.result[LD.clump.result$eth.vec!="EUR",]
+  method_vec <- rep("P+T",nrow(LD.clump.result))
+  LD.clump.result$method_vec = method_vec
+  LD.clump.result.PT <- LD.clump.result
+  load("best_eur_snp_result.rdata")
+  LD.clump.result$method_vec <- "Best EUR PRS"
+  LD.clump.result <- rbind(LD.clump.result,LD.clump.result.PT)
+  for(l in 1:3){
+    idx <- which(LD.clump.result$l_vec==l)
+    cau_vec[idx] <- paste0("Causal SNPs Proportion = ",csp[l])
+  }
+  cau_vec <- factor(cau_vec,
+                    levels = paste0("Causal SNPs Proportion = ",csp))
+  
+  LD.clump.result <- cbind(LD.clump.result,sample_size,cau_vec)
+  
+  
+  p <- ggplot(LD.clump.result,aes(x= sample_size,y=r2.vec,group=method_vec))+
+    geom_line(aes(color=method_vec))+
+    geom_point(aes(color=method_vec))+
+    theme_Publication()+
+    ylab("R2")+
+    xlab("Sample Size")+
+    guides(color=guide_legend(title="Method"))+
+    facet_grid(vars(cau_vec),vars(eth.vec))+
+    scale_color_nejm()
+  png(file = paste0("./LD_clumping_result_p_thres.png"),
+      width = 15, height = 10, res = 300,units = "in")
+  p
+  dev.off()
+  
+  dev.off()
+  
