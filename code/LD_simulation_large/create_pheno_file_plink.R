1aba35680c14a86c1d842465ccab650a52a0c2dc
Zhang <andrew.haoyu@gmail.com>
2020-08-24 10:36:46 -0400

    create_pheno_file_plink

--- code/LD_simulation_large/create_pheno_file_plink.R
+++ code/LD_simulation_large/create_pheno_file_plink.R
@@ -1,24 +1,22 @@
 #create eht phenotypes file for plink to run
 #i for ethnic group
 #l for causal proportion
-#k for replicates
 args = commandArgs(trailingOnly = T)
 i = as.numeric(args[[1]])
 l = as.numeric(args[[2]])
-k = as.numeric(args[[3]])
+
 eth <- c("EUR","AFR","AMR","EAS","SAS")
 cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
-
-cut.point <- seq(1,101,20)
-
 library(data.table)    
 all.pheno <- as.data.frame(fread(paste0(cur.dir,eth[i],"/phenotypes_rho",l,".phen")))
 n.train <- c(15000,45000,80000,100000)
 fam <- as.data.frame(fread(paste0(cur.dir,eth[i],"/all_chr.tag.fam")))
 n <- nrow(fam)
 pheno <- fam[,1:2]
-for(i_rep in cut.point[k]:(cut.point[k+1]-1)){
-  print(i_rep)
+#for(i_rep in cut.point[k]:(cut.point[k+1]-1)){
+for(i_rep in 1:100){ 
+ print(i_rep)
+  pheno <- NULL
   for(m in 1:length(n.train)){
     #only use the first set of all pheno as training
     #all the others are used for testing
