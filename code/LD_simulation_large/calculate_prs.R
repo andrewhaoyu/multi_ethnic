3f7b3245838feafd9e9d65af100744e38172f06f
Zhang <andrew.haoyu@gmail.com>
2020-09-01 21:40:43 -0400

    calculate_prs

--- code/LD_simulation_large/calculate_prs.R
+++ code/LD_simulation_large/calculate_prs.R
@@ -24,17 +24,23 @@ setwd("/data/zhangh24/multi_ethnic/")
 
     LD <- as.data.frame(fread(paste0(cur.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep,".clumped")))
     clump.snp <- LD[,3,drop=F]  
-    sum.data <- as.data.frame(fread(paste0("./result/LD_simulation_new/",eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,)))  
+    sum.data <- as.data.frame(fread(paste0("./result/LD_simulation_new/",eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep)))  
     colnames(sum.data)[2] <- "SNP"
     prs.all <- left_join(clump.snp,sum.data,by="SNP") 
     #for(k in 1:length(pthres)){
     prs.file <- prs.all %>% filter(P<=pthres[k]) %>% 
       select(SNP,A1,BETA)
-    write.table(prs.file,file = paste0(cur.dir,eth[i],"/prs/prs_file_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep),col.names = T,row.names = F,quote=F)
+    sid <- Sys.getenv("SLURM_JOB_ID")
+    dir.create(paste0('/lscratch/',sid,'/',eth[i],"/"),showWarnings = F)
+    temp.dir <- paste0('/lscratch/',sid,'/',eth[i],"/")
+    #setwd(temp.dir)
+    write.table(prs.file,file = paste0(temp.dir,"prs_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep),col.names = T,row.names = F,quote=F)
+    system(paste0("cp ",cur.dir,eth[i],"/chr",j,".tag.* ",temp.dir,"."))
+    system(paste0("ls ",temp.dir))
     #}
     #for(j in 1:22){
     if(nrow(prs.file)>0){
-      system(paste0("/data/zhangh24/software/plink2 --threads 2 --score ",cur.dir,eth[i],"/prs/prs_rho_",l,"_size_",m,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",cur.dir,eth[i],"/chr",j,".tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_rho_",l,"_size_",m,"_",j,"_rep_",i_rep))
+      system(paste0("/data/zhangh24/software/plink2 --threads 2 --score ",temp.dir,"prs_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",temp.dir,"chr",j,".tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_rho_",l,"_size_",m,"_chr_",j,"_rep_",i_rep))
        #system(paste0("/data/zhangh24/software/plink2 --score ",cur.dir,eth[i],"/prs/prs_file_pvalue_",k,"_rho_",l,"_size_",m,,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",cur.dir,eth[i],"/all_chr.tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_",k,"_rho_",l,"_size_",m))
     }
     
