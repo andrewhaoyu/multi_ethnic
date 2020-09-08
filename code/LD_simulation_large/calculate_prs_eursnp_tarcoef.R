e846c0e5f86894f7b91490ebef0273e398b2bf8b
Zhang <andrew.haoyu@gmail.com>
2020-09-08 17:05:58 -0400

    updateg

--- code/LD_simulation_large/calculate_prs_eursnp_tarcoef.R
+++ code/LD_simulation_large/calculate_prs_eursnp_tarcoef.R
@@ -28,7 +28,7 @@ for(l in 1:3){
     load(paste0(cur.dir,"LD.clump.result.rdata"))
     load("/data/zhangh24/multi_ethnic/result/LD_simulation/r2.mat.eur.rdata")
     #keep the EUR results with sample size at 100,000 (m = 4)
-    r2.mat <- as.data.frame(LD.result.list[[2]]) %>% 
+    r2.mat <- as.data.frame(LD.result.list[[3]]) %>% 
       filter(eth.vec=="EUR"&
                m_vec==4&
                l_vec==l&
@@ -57,7 +57,7 @@ for(l in 1:3){
     system(paste0("rm ",cur.dir,eth[i],"/prs/prs_eursnp_tarcoef_rho_",l,"_size_",m,"_",j,"_rep_",i_rep,".log"))
     system(paste0("rm ",cur.dir,eth[i],"/prs/prs_eursnp_tarcoef_rho_",l,"_size_",m,"_",j,"_rep_",i_rep,".nosex"))
     system(paste0("rm ",cur.dir,eth[i],"/prs/prs_eursnp_tarcoef_rho_",l,"_size_",m,"_",j,"_rep_",i_rep,".nopred"))
-    
+    gc()
   }
   }
 #n.snp.mat <- matrix(0,length(pthres),4)
