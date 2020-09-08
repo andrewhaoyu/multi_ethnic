9ff039c2850fe043d48e58ee0fac1706027a8942
Zhang <andrew.haoyu@gmail.com>
2020-09-08 17:03:26 -0400

    update

--- code/LD_simulation_large/calculate_prs_eursnp_eurcoef.R
+++ code/LD_simulation_large/calculate_prs_eursnp_eurcoef.R
@@ -6,7 +6,11 @@ args = commandArgs(trailingOnly = T)
 i_rep = as.numeric(args[[1]])
 i = as.numeric(args[[2]])
 j = as.numeric(args[[3]])
+setwd("/data/zhangh24/multi_ethnic/")
 cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
+eth <- c("EUR","AFR","AMR","EAS","SAS")
+pthres <- c(5E-08,1E-07,5E-07,1E-06,5E-06,1E-05,5E-05,1E-04,1E-03,1E-02,1E-01)
+
 sid <- Sys.getenv("SLURM_JOB_ID")
 dir.create(paste0('/lscratch/',sid,'/',eth[i],"/"),showWarnings = F)
 temp.dir <- paste0('/lscratch/',sid,'/',eth[i],"/")
@@ -14,15 +18,11 @@ system(paste0("cp ",cur.dir,eth[i],"/chr",j,".tag.* ",temp.dir,"."))
 system(paste0("ls ",temp.dir))
 library(dplyr)
 library(data.table)
-eth <- c("EUR","AFR","AMR","EAS","SAS")
-pthres <- c(5E-08,1E-07,5E-07,1E-06,5E-06,1E-05,5E-05,1E-04,1E-03,1E-02,1E-01)
 
 for(l in 1:3){
   for(m in 1:4){
     #n.snp.mat <- matrix(0,length(pthres),4)
     #load EUR r2 result
-    setwd("/data/zhangh24/multi_ethnic/")
-    
     load(paste0(cur.dir,"LD.clump.result.rdata"))
     
     #load("/data/zhangh24/multi_ethnic/result/LD_simulation/r2.mat.eur.rdata")
