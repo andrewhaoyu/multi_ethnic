d9e45998d093512f103bd8add63ecb118f4f88f2
Zhang <andrew.haoyu@gmail.com>
2020-09-11 11:47:12 -0400

    update

---
+++ code/LD_simulation_large/theme_publication.R
@@ -0,0 +1,42 @@
+theme_Publication <- function(base_size=12) {
+  library(grid)
+  library(ggthemes)
+  (theme_foundation(base_size=base_size, )
+    + theme(plot.title = element_text(face = "bold",
+                                      size = rel(1.1), hjust = 0.5),
+            text = element_text(),
+            panel.background = element_rect(colour = NA),
+            plot.background = element_rect(colour = NA),
+            panel.border = element_rect(colour = NA),
+            axis.title = element_text(face = "bold",size = rel(1)),
+            axis.title.y = element_text(angle=90,vjust =2),
+            axis.title.x = element_text(vjust = -0.2),
+            axis.text = element_text(), 
+            axis.line = element_line(colour="black"),
+            axis.ticks = element_line(),
+            panel.grid.major = element_line(colour="#f0f0f0"),
+            panel.grid.minor = element_line(colour="#f0f0f0"),
+            legend.key = element_rect(colour = NA),
+            #legend.position = "bottom",
+            #legend.direction = "horizontal",
+            #legend.key.size= unit(0.2, "cm"),
+            #legend.margin = unit(0, "cm"),
+            legend.title = element_text(face="bold.italic"),
+            plot.margin=unit(c(10,5,5,5),"mm"),
+            strip.background=element_rect(colour="#f0f0f0",fill="#f0f0f0"),
+             strip.text = element_text(face="bold")
+    ))
+  
+}
+
+scale_fill_Publication <- function(...){
+  library(scales)
+  discrete_scale("fill","Publication",manual_pal(values = c("#386cb0","#EF7E3D","#FFD042","#7fc97f","#004953","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+scale_colour_Publication <- function(...){
+  library(scales)
+  discrete_scale("colour","Publication",manual_pal(values = c("#386cb0","#EF7E3D","chartreuse4","#7fc97f","#004953","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33")), ...)
+  
+}
+
