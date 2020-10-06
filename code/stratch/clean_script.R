075077c44cb121375f52830c90eafd2095fd5c6a
Zhang <andrew.haoyu@gmail.com>
2020-10-06 15:43:52 -0400

    clean

--- code/stratch/clean_script.R
+++ code/stratch/clean_script.R
@@ -1,10 +1,25 @@
 #clean unnecessary files
 eth <- c("EUR","AFR","AMR","EAS","SAS")
 cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
+
+
+
 for(i in 2:5){
-  for(k1 in 1:12){
-    system(paste0("rm ",cur.dir,eth[i],"/prs/prs_two_dim_",k1,"_*.log"))
-    system(paste0("rm ",cur.dir,eth[i],"/prs/prs_two_dim_",k1,"_*.nosex"))
-    system(paste0("rm ",cur.dir,eth[i],"/prs/prs_two_dim_",k1,"_*.nopred"))
-  }
+  system(paste0("rm ",cur.dir,eth[i],"/*.assoc.linear"))
+    system(paste0("rm ",cur.dir,eth[i],"/prs/*.log"))
+    system(paste0("rm ",cur.dir,eth[i],"/prs/*.nosex"))
+    system(paste0("rm ",cur.dir,eth[i],"/prs/*.nopred"))
+ 
+}
+
+
+cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation/"
+for(i in 1:5){
+  system(paste0("rm ",cur.dir,eth[i],"/*.assoc.linear"))
+  system(paste0("rm ",cur.dir,eth[i],"/*.log"))
+  system(paste0("rm ",cur.dir,eth[i],"/*.nosex"))
+  system(paste0("rm ",cur.dir,eth[i],"/prs/*.log"))
+  system(paste0("rm ",cur.dir,eth[i],"/prs/*.nosex"))
+  system(paste0("rm ",cur.dir,eth[i],"/prs/*.nopred"))
+  
 }
