e8dc4d6b456d92c06a655a0c06e525c9ef841156
Zhang <andrew.haoyu@gmail.com>
2020-10-13 11:22:34 -0400

    calcualte_prs

--- code/LD_simulation_GA/calculate_prs.R
+++ code/LD_simulation_GA/calculate_prs.R
@@ -29,6 +29,7 @@ dir.create(paste0('/lscratch/',sid,'/',eth[i],"/"),showWarnings = F)
 temp.dir <- paste0('/lscratch/',sid,'/',eth[i],"/")
 system(paste0("cp ",cur.dir,eth[i],"/chr",j,".tag.* ",temp.dir,"."))
 system(paste0("ls ",temp.dir))
+print("step1 finished")
 #for(l in 1:3){
  # for(m in 1:4){
     
@@ -48,7 +49,7 @@ system(paste0("ls ",temp.dir))
       write.table(prs.file,file = paste0(temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1),col.names = T,row.names = F,quote=F)
       #}
       #for(j in 1:22){
-      
+      print("step2 finished")
       if(nrow(prs.file)>0){
         system(paste0("/data/zhangh24/software/plink2 --threads 2 --score ",temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1," no-sum no-mean-imputation --bfile ",temp.dir,"chr",j,".tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",out.dir,eth[i],"/prs/prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_chr_",j,"_rep_",i_rep,"_GA_",i1))
         #system(paste0("/data/zhangh24/software/plink2 --score ",cur.dir,eth[i],"/prs/prs_file_pvalue_",k,"_rho_",l,"_size_",m,,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",cur.dir,eth[i],"/all_chr.tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_",k,"_rho_",l,"_size_",m))
@@ -59,4 +60,5 @@ system(paste0("ls ",temp.dir))
     
 #  }
 #}
-system(paste0('rm -r /lscratch/',sid,'/',eth[i],'/'))
+print("step3 finished")
+    system(paste0('rm -r /lscratch/',sid,'/',eth[i],'/'))
