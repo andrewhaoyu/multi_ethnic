786486bbae51de728f09fa4860e749882a0793b5
Zhang <andrew.haoyu@gmail.com>
2020-10-14 13:05:18 -0400

    update

--- code/LD_simulation_GA/run_gwas.R
+++ code/LD_simulation_GA/run_gwas.R
@@ -27,7 +27,7 @@ system(paste0("cp ", cur.dir,eth[i],"/chr",j,".tag.bed /lscratch/",sid,"/test/",
 system(paste0("cp ", cur.dir,eth[i],"/chr",j,".tag.bim /lscratch/",sid,"/test/",eth[i],"_chr",j,".tag.bim"))
 system(paste0("cp ", cur.dir,eth[i],"/chr",j,".tag.fam /lscratch/",sid,"/test/",eth[i],"_chr",j,".tag.fam"))
 
-system(paste0("/data/zhangh24/software/plink2 --threads 2 --bfile /lscratch/",sid,"/test/",eth[i],"_chr",j,".tag --out ",out.dir,eth[i],"/summary_chr_",j,"_rho_",l,"_rep_",i_rep,"_GA_",i1,".out --linear --all-pheno --allow-no-sex --pheno ",out.dir,eth[i],"/pheno_plink_rho_",l,"_rep_",i_rep,"_GA_",i1))
+res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --bfile /lscratch/",sid,"/test/",eth[i],"_chr",j,".tag --out ",out.dir,eth[i],"/summary_chr_",j,"_rho_",l,"_rep_",i_rep,"_GA_",i1,".out --linear --all-pheno --allow-no-sex --pheno ",out.dir,eth[i],"/pheno_plink_rho_",l,"_rep_",i_rep,"_GA_",i1))
 
 system(paste0('rm -rf /lscratch/',sid,'/test'))
 # for(i in 1:5){
---
+++ code/LD_simulation_GA/test.R
@@ -0,0 +1,8 @@
+args = commandArgs(trailingOnly = T)
+i1 = as.numeric(args[[1]])
+
+if(i1 ==1){
+  stop()
+}else{
+  a = 1
+}
