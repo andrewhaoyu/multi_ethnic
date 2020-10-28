1463b50ee6877e62fecae852332b20b41e107ead
Zhang <andrew.haoyu@gmail.com>
2020-10-28 17:02:37 -0400

    compres

--- code/stratch/compress_summary_file.R
+++ code/stratch/compress_summary_file.R
@@ -7,9 +7,13 @@ cur.dir <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i1]
 #system(paste0("rm -rf summary_out_rep"))
 # system(paste0("rm -rf ",cur.dir,"summary_out_GA"))
 # system(paste0("mkdir ",cur.dir,"summary_out_GA"))
-system(paste0("mkdir ",cur.dir,"pheno_GA"))
-system(paste0("cp ",cur.dir,"*.phen ",cur.dir,"pheno_GA/"))
-res = system(paste0("tar -zcvf pheno_GA.tar.gz ",cur.dir,"pheno_GA/"))
+# system(paste0("mkdir ",cur.dir,"pheno_GA"))
+# system(paste0("cp ",cur.dir,"*.phen ",cur.dir,"pheno_GA/"))
+res = system(paste0("cd ",cur.dir," ; tar -zcvf pheno_GA.tar.gz pheno_GA/"))
+if(res==2){
+  stop()
+}
+res = system(paste0("cd ",cur.dir," ;tar -zcvf summary_out_GA.tar.gz summary_out_GA/"))
 if(res==2){
   stop()
 }
