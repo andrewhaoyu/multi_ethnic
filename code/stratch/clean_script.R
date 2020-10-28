ccc03e7ba4e01cd8ea9e74ae26a3097451650f89
Zhang <andrew.haoyu@gmail.com>
2020-10-28 18:41:22 -0400

    clean

--- code/stratch/clean_script.R
+++ code/stratch/clean_script.R
@@ -1,16 +1,22 @@
 #clean unnecessary files
 eth <- c("EUR","AFR","AMR","EAS","SAS")
-cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
+cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
 
 #system(paste0("rm -rf /data/zhangh24/multi_ethnic/result/LD_simulation"))
 
 for(i in 1:5){
   #system(paste0("rm -r ",cur.dir,eth[i],"/prs"))
   #system(paste0("mkdir ",cur.dir,eth[i],"/prs"))
-    system(paste0("rm ",cur.dir,eth[i],"/*.log"))
-    system(paste0("rm ",cur.dir,eth[i],"/*.nosex"))
+  system(paste0("rm -rf ",cur.dir,eth[i],"/prs"))
+  system(paste0("rm ",cur.dir,eth[i],"/*.clumped"))
+    system(paste0("rm ",cur.dir,eth[i],"/r2.list*"))
    # system(paste0("rm ",cur.dir,eth[i],"/*.clumped"))
-    system(paste0("rm ",cur.dir,eth[i],"/*.assoc.linear"))
+    system(paste0("rm ",cur.dir,eth[i],"/summary_out_*"))
+    
+    system(paste0("rm ",cur.dir,eth[i],"/pheno_summary_stat.tar.gz*"))
+    system(paste0("rm -rf ",cur.dir,eth[i],"/pheno_summary_stat"))
+    system(paste0("rm -rf ",cur.dir,eth[i],"/pheno_plink_*"))
+    system(paste0("rm -rf ",cur.dir,eth[i],"/*.phen"))
     #system(paste0("rm ",cur.dir,eth[i],"/prs/*.log"))
     #system(paste0("rm ",cur.dir,eth[i],"/prs/*.nosex"))
     #system(paste0("rm ",cur.dir,eth[i],"/prs/*.nopred"))
