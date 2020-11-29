ab596710a610384dbf3089041e3e918486e55570
Zhang <andrew.haoyu@gmail.com>
2020-11-29 14:28:06 -0500

    calculate prs stack

--- code/LD_simulation_GA/LD_stack/calculate_prs_stack.r
+++ code/LD_simulation_GA/LD_stack/calculate_prs_stack.r
@@ -27,18 +27,20 @@ out.dir <-  "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/LD_stack/"
 sid <- Sys.getenv("SLURM_JOB_ID")
 dir.create(paste0('/lscratch/',sid,'/',eth[i],"/"),showWarnings = F)
 temp.dir <- paste0('/lscratch/',sid,'/',eth[i],"/")
-system(paste0("cp ",cur.dir,eth[i],"/chr",j,".tag.* ",temp.dir,"."))
+system(paste0("cp ",cur.dir,eth[i],"/chr",j,".mega.* ",temp.dir,"."))
 system(paste0("ls ",temp.dir))
 print("step1 finished")
 #for(l in 1:3){
 r2_vec = c(0.01,0.05,0.1,0.2,0.5)
 wc_base_vec = c(50,100,200,500)
-for(r_ind in 1:length(r2_vec)){
-  wc_vec = wc_base_vec/r2_vec[r_ind]
-  for(w_ind in 1:length(wc_vec)){
     
 for(m in 1:4){
-  
+  sum.data <- as.data.frame(fread(paste0("./result/LD_simulation_GA/",eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1)))  
+  for(r_ind in 1:length(r2_vec)){
+    wc_vec = wc_base_vec/r2_vec[r_ind]
+    for(w_ind in 1:length(wc_vec)){
+      print(c(r_ind,w_ind))
+      
   setwd("/data/zhangh24/multi_ethnic/")
   
   LD <- as.data.frame(fread(paste0(out.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind,".clumped")))
@@ -46,19 +48,19 @@ for(m in 1:4){
   #here use summary_out or summary_MAF_out doesn't make difference
   #since the LD_clumping step only include MAF_out
   
-  sum.data <- as.data.frame(fread(paste0("./result/LD_simulation_GA/",eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1)))  
+  
   colnames(sum.data)[2] <- "SNP"
   prs.all <- left_join(clump.snp,sum.data,by="SNP") 
   for(k in 1:length(pthres)){
-    prs.file <- prs.all %>% filter(P<=pthres[k]&CHR==j)%>% 
+    prs.file <- prs.all %>% filter(P<=pthres[k]&CHR==j) %>% 
       select(SNP,A1,BETA)
     #setwd(temp.dir)
-    write.table(prs.file,file = paste0(temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1),col.names = T,row.names = F,quote=F)
+    write.table(prs.file,file = paste0(temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind),col.names = T,row.names = F,quote=F)
     #}
     #for(j in 1:22){
     print("step2 finished")
     if(nrow(prs.file)>0){
-      res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --score ",temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1," no-sum no-mean-imputation --bfile ",temp.dir,"chr",j,".tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i],"/duplicated.id  --out ",temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_chr_",j,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind))
+      res <- system(paste0("/data/zhangh24/software/plink2 --threads 2 --score ",temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind," no-sum no-mean-imputation --bfile ",temp.dir,"chr",j,".mega --exclude /data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i],"/duplicated.id  --out ",temp.dir,"prs_chr_",j,"_pvalue_",k,"_rho_",l,"_size_",m,"_chr_",j,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind))
       #system(paste0("/data/zhangh24/software/plink2 --score ",cur.dir,eth[i],"/prs/prs_file_pvalue_",k,"_rho_",l,"_size_",m,,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",cur.dir,eth[i],"/all_chr.tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_",k,"_rho_",l,"_size_",m))
       if(res==2){
         stop()
@@ -68,13 +70,14 @@ for(m in 1:4){
     
     
   }
+  system(paste0("rm ",temp.dir,"prs_chr_*"))
   
 }
   }
 }
 #}
 print("step3 finished")
-#system(paste0('rm -r /lscratch/',sid,'/',eth[i],'/'))
+system(paste0('rm -r /lscratch/',sid,'/',eth[i],'/'))
 
 
 
