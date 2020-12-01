0401bf637a94b55b134c1c2566405847306ca421
Zhang <andrew.haoyu@gmail.com>
2020-12-01 12:03:08 -0500

    calcualte_prs_stack

--- code/LD_simulation_GA/LD_stack/calculate_prs_stack.r
+++ code/LD_simulation_GA/LD_stack/calculate_prs_stack.r
@@ -36,7 +36,7 @@ print("step1 finished")
 r2_vec = c(0.01,0.05,0.1,0.2,0.5)
 wc_base_vec = c(50,100,200,500)
 setwd("/data/zhangh24/multi_ethnic/")  
-for(m in 1:1){
+for(m in 1:4){
   sum.data <- as.data.frame(fread(paste0("./result/LD_simulation_GA/",eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1)))  
   for(r_ind in 1:length(r2_vec)){
     wc_vec = wc_base_vec/r2_vec[r_ind]
@@ -80,7 +80,7 @@ for(m in 1:1){
   q_range = q_range[1:(temp-1),]
   write.table(q_range,file = paste0(temp.dir.prs,"q_range_file"),row.names = F,col.names = F,quote=F)
   res <- system(paste0("/data/zhangh24/software/plink2 --q-score-range ",temp.dir.prs,"q_range_file ",temp.dir.prs,"p_value_file header --threads 2 --score ",temp.dir.prs,"prs_file header no-sum no-mean-imputation --bfile ",temp.dir,"chr",j,".mega --exclude /data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i],"/duplicated.id  --out ",temp.dir.prs,"prs_chr_",j,"_rho_",l,"_size_",m,"_chr_",j,"_rep_",i_rep,"_GA_",i1,"_rind_",r_ind,"_wcind_",w_ind))
-  system(paste0("ls ",temp.dir.prs))
+  #system(paste0("ls ",temp.dir.prs))
   #system(paste0("/data/zhangh24/software/plink2 --score ",cur.dir,eth[i],"/prs/prs_file_pvalue_",k,"_rho_",l,"_size_",m,,"_rep_",i_rep," no-sum no-mean-imputation --bfile ",cur.dir,eth[i],"/all_chr.tag --exclude /data/zhangh24/multi_ethnic/result/LD_simulation/",eth[i],"/duplicated.id  --out ",cur.dir,eth[i],"/prs/prs_",k,"_rho_",l,"_size_",m))
   print("step2 finished")
   if(res==2){
