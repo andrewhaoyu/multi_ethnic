8074fb5dc6d01ff6e29fdf3f1910762811d8a19f
Zhang <andrew.haoyu@gmail.com>
2020-12-10 23:17:18 -0500

    compress

--- code/LD_simulation_GA/merge_gwas_summary.R
+++ code/LD_simulation_GA/merge_gwas_summary.R
@@ -9,9 +9,9 @@ eth <- c("EUR","AFR","AMR","EAS","SAS")
 #three different causal proportion l
 #three different training sample sizes m
 #22 chr
-code <- c("c",2*5*3*10*4)
+code <- c("c",3*5*3*10*4)
 temp = 1
-for(i1 in 1:2){
+for(i1 in 3:5){
   for(i in 1:5){
     for(l in 1:3){
       for(i_rep in 1:10){
@@ -22,7 +22,8 @@ for(i1 in 1:2){
             temp.code <- paste0(temp.code, " ",cur.dir,eth[i],"/summary_chr_",j,"_rho_",l,"_rep_",i_rep,"_GA_",i1,".out.P",m,".assoc.linear")
           }
           temp.code <- paste0(temp.code," >> ",cur.dir,eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1)
-          code[temp] <- temp.code
+          print(i_rep)
+          code[temp] <- system(paste0(temp.code))
           temp <- temp+1
         }
       }
@@ -30,7 +31,7 @@ for(i1 in 1:2){
   }
   }
 
-write.table(code,file = "/data/zhangh24/multi_ethnic/code/LD_simulation_GA/merge_gwas_summary.sh",row.names = F,col.names = F,quote=F)
+#write.table(code,file = "/data/zhangh24/multi_ethnic/code/LD_simulation_GA/merge_gwas_summary.sh",row.names = F,col.names = F,quote=F)
 
 
 
--- code/stratch/compress_summary_file.R
+++ code/stratch/compress_summary_file.R
@@ -1,8 +1,8 @@
 #
 args = commandArgs(trailingOnly = T)
-i1 = as.numeric(args[[1]])
+i = as.numeric(args[[1]])
 eth <- c("EUR","AFR","AMR","EAS","SAS")
-cur.dir <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i1],"/")
+cur.dir <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i],"/")
 #system(paste0("cd /data/zhangh24/multi_ethnic/result/LD_simulation_new/",eth[i1],"/"))
 #system(paste0("rm -rf summary_out_rep"))
 # system(paste0("rm -rf ",cur.dir,"summary_out_GA"))
@@ -10,8 +10,12 @@ cur.dir <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i1]
 # system(paste0("mkdir ",cur.dir,"pheno_GA"))
 # system(paste0("cp ",cur.dir,"*.phen ",cur.dir,"pheno_GA/"))
 system(paste0("mkdir ",cur.dir,"pheno_summary_out_GA"))
-system(paste0("cd ",cur.dir," ;mv pheno_GA/* pheno_summary_out_GA/"))
-system(paste0("cd ",cur.dir," ;mv summary_out_GA/* pheno_summary_out_GA/"))
+for(i1 in 3:5){
+  system(paste0("cd ",cur.dir," ;cp phenotypes_rho*_",i1,".phen pheno_summary_out_GA/"))
+  system(paste0("cd ",cur.dir," ;cp summary_out_rho*_GA_",i1," pheno_summary_out_GA/"))
+}
+#system(paste0("cd ",cur.dir," ;mv pheno_GA/* pheno_summary_out_GA/"))
+#system(paste0("cd ",cur.dir," ;mv summary_out_GA/* pheno_summary_out_GA/"))
 res = system(paste0("cd ",cur.dir," ; tar -zcvf pheno_summary_out_GA.tar.gz pheno_summary_out_GA/"))
 if(res==2){
   stop()
