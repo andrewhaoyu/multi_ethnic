91d9468cf5356aebd664acabccbb030b11729af4
Zhang <andrew.haoyu@gmail.com>
2020-12-11 15:52:30 -0500

    update

--- code/stratch/uncompress_file.R
+++ code/stratch/uncompress_file.R
@@ -2,7 +2,7 @@
 args = commandArgs(trailingOnly = T)
 i1 = as.numeric(args[[1]])
 eth <- c("EUR","AFR","AMR","EAS","SAS")
-cur.dir <- paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic/LD_simulation_new/",eth[i1],"/")
+cur.dir <- paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic/LD_simulation_new/",eth[i1],"/temp/")
 #system(paste0("cd /data/zhangh24/multi_ethnic/result/LD_simulation_new/",eth[i1],"/"))
 #system(paste0("rm -rf summary_out_rep"))
 # system(paste0("rm -rf ",cur.dir,"summary_out_GA"))
