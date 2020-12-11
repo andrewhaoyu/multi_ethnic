730f25ea7052ff1517d157ec362786c7a908db8c
Zhang <andrew.haoyu@gmail.com>
2020-12-11 13:26:39 -0500

    compress

--- code/stratch/compress_summary_file.R
+++ code/stratch/compress_summary_file.R
@@ -9,6 +9,8 @@ cur.dir <- paste0("/data/zhangh24/multi_ethnic/result/LD_simulation_GA/",eth[i],
 # system(paste0("mkdir ",cur.dir,"summary_out_GA"))
 # system(paste0("mkdir ",cur.dir,"pheno_GA"))
 # system(paste0("cp ",cur.dir,"*.phen ",cur.dir,"pheno_GA/"))
+system(paste0("rm -rf ",cur.dir,"pheno_summary_out_GA"))
+system(paste0("rm ",cur.dir,"pheno_summary_out_GA.tar.gz"))
 system(paste0("mkdir ",cur.dir,"pheno_summary_out_GA"))
 for(i1 in 3:5){
   system(paste0("cd ",cur.dir," ;cp phenotypes_rho*_",i1,".phen pheno_summary_out_GA/"))
