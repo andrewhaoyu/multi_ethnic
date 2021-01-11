2530eea5fc5095a72e5220c0c47244c36b068bf4
Zhang <andrew.haoyu@gmail.com>
2021-01-11 09:45:42 -0500

    update

--- code/ARIC/LD_clumping.R
+++ code/ARIC/LD_clumping.R
@@ -8,9 +8,9 @@ args = commandArgs(trailingOnly = T)
 #i_rep represent simulation replication
 #i1 represent the genetic architecture
 
-i = as.numeric(args[[1]])
-l = as.numeric(args[[2]])
-j = as.numeric(args[[3]])
+#i = as.numeric(args[[1]])
+#l = as.numeric(args[[2]])
+j = as.numeric(args[[1]])
 #m = as.numeric(args[[3]])
 #i_rep = as.numeric(args[[4]])
 #i1 = as.numeric(args[[5]])
@@ -18,39 +18,45 @@ library(data.table)
 #install.packages("dplyr")
 #install.packages("vctrs")
 library(dplyr)
+
 eth <- c("EUR","AFR","AMR","EAS","SAS")
 trait = c("eGFRcr","ACR","urate")
-setwd("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic")
-temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[l],"/",eth[i],"/")
-data.dir = "/dcl01/chatterj/data/jin/prs/realdata/ARIC/"
-out.dir = paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic/result/ARIC/",trait[l],"/",eth[i],"/")
-#load gwas summary statistics
-sum.data = as.data.frame(fread(paste0(data.dir,trait[l],"/",eth[i],"/sumdata/training-GWAS-formatted.txt")))
-# write.table(sum.data.MAF,file = paste0("/lscratch/",sid,"/test/",eth[i],"_summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,".out")
-#             ,col.names = T,row.names = F,quote=F) 
-#prepare association file for plink
-sum.data.assoc = sum.data %>% 
-  mutate(BP=POS,SNP = SNP_ID,A1 = REF,
-         P = PVAL) %>% 
-  filter(CHR==j) 
-
-sum.data.assoc = sum.data.assoc[,c("CHR","SNP","BP","A1","BETA","P")]
-#idx <- which(sum.data.assoc$SNP=="rs4970836")
-write.table(sum.data.assoc,file = paste0(temp.dir,"chr_",j,"_assoc.txt"),col.names = T,row.names = F,quote=F)
-
-# dim(summary)
-# head(summary)
-pthr = 0.5
-r2thr = 0.1
-kbpthr = 500
-eth <- c("EUR","AFR","AMR","EAS","SAS")
-#cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
-#code <- rep("c",5*3*3)
-#system(paste0("/data/zhangh24/software/plink2 --bfile /data/zhangh24/KG.plink/",eth[i],"/chr_all --clump ",cur.dir,eth[i],"/summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,".out --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",cur.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep))
-res = system(paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/plink --bfile ",data.dir,trait[l],"/",eth[i],"/geno/mega/ref_chr",j," --clump ",temp.dir,"chr_",j,"_assoc.txt --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",temp.dir,"LD_clump_chr_",j))
-system(paste0("mv ",temp.dir,"LD_clump_chr_",j,".clumped ",out.dir))
-if(res==2){
-  stop()
+for(i in 1:2){
+  for(l in 1:3){
+    setwd("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic")
+    temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[l],"/",eth[i],"/")
+    data.dir = "/dcl01/chatterj/data/jin/prs/realdata/ARIC/"
+    out.dir = paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic/result/ARIC/",trait[l],"/",eth[i],"/")
+    #load gwas summary statistics
+    sum.data = as.data.frame(fread(paste0(data.dir,trait[l],"/",eth[i],"/sumdata/training-GWAS-formatted.txt")))
+    # write.table(sum.data.MAF,file = paste0("/lscratch/",sid,"/test/",eth[i],"_summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,".out")
+    #             ,col.names = T,row.names = F,quote=F) 
+    #prepare association file for plink
+    sum.data.assoc = sum.data %>% 
+      mutate(BP=POS,SNP = SNP_ID,A1 = REF,
+             P = PVAL) %>% 
+      filter(CHR==j) 
+    
+    sum.data.assoc = sum.data.assoc[,c("CHR","SNP","BP","A1","BETA","P")]
+    #idx <- which(sum.data.assoc$SNP=="rs4970836")
+    write.table(sum.data.assoc,file = paste0(temp.dir,"chr_",j,"_assoc.txt"),col.names = T,row.names = F,quote=F)
+    
+    # dim(summary)
+    # head(summary)
+    pthr = 0.5
+    r2thr = 0.1
+    kbpthr = 500
+    eth <- c("EUR","AFR","AMR","EAS","SAS")
+    #cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
+    #code <- rep("c",5*3*3)
+    #system(paste0("/data/zhangh24/software/plink2 --bfile /data/zhangh24/KG.plink/",eth[i],"/chr_all --clump ",cur.dir,eth[i],"/summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,".out --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",cur.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep))
+    res = system(paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/plink --bfile ",data.dir,trait[l],"/",eth[i],"/geno/mega/ref_chr",j," --clump ",temp.dir,"chr_",j,"_assoc.txt --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",temp.dir,"LD_clump_chr_",j))
+    system(paste0("mv ",temp.dir,"LD_clump_chr_",j,".clumped ",out.dir))
+    if(res==2){
+      stop()
+    }
+    
+  }
 }
 #system(paste0("rm ",cur.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,".log"))
 
