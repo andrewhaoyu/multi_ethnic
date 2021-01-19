0cc4014af982f4db7e275a788edba0ea61a3b5ae
Zhang <andrew.haoyu@gmail.com>
2021-01-19 13:43:18 -0500

    update

--- code/ARIC/calculate_grm.R
+++ code/ARIC/calculate_grm.R
@@ -1,6 +1,11 @@
 args = commandArgs(trailingOnly = T)
+#i is the ethnic
+#j is the number of chromsome
 i = as.numric(args[[1]])
 j = as.numeric(args[[2]])
+
+eth <- c("EUR","AFR","AMR","EAS","SAS")
+trait = c("eGFRcr","ACR","urate")
 
 setwd("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic")
 temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[l],"/",eth[i],"/")
