ae1db71b094713081a2984f5f231d815dd9a818d
Zhang <andrew.haoyu@gmail.com>
2021-01-19 15:36:26 -0500

    maf 0.05

--- code/ARIC/calculate_grm.R
+++ code/ARIC/calculate_grm.R
@@ -10,4 +10,4 @@ trait = c("eGFRcr","ACR","urate")
 setwd("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/multi_ethnic")
 temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[1],"/",eth[i],"/")
 data.dir = "/dcl01/chatterj/data/jin/prs/realdata/ARIC/"
-res <- system(paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/gcta/gcta64 --bfile ",data.dir,trait[1],"/",eth[i],"/geno/mega/chr.qc",j," --maf 0.01 --make-grm --out ",temp.dir,"chr.qc",j))
+res <- system(paste0("/dcl01/chatterj/data/hzhang1/multi_ethnic_data_analysis/gcta/gcta64 --bfile ",data.dir,trait[1],"/",eth[i],"/geno/mega/chr.qc",j," --maf 0.05 --make-grm --out ",temp.dir,"chr.qc",j))
