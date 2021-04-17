6032bdcb70920c890e33061a9deddf5fdd8ca29e
Zhang <andrew.haoyu@gmail.com>
2021-04-17 16:04:31 -0400

    update

--- code/breast_cancer/convert_vcf_to_plink.R
+++ code/breast_cancer/convert_vcf_to_plink.R
@@ -6,15 +6,18 @@ j = as.numeric(args[[1]])
 #then use plink to convert bcf to plink
 #remove all the multi-allelic snps
 library(data.table)
+system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j," --maf 0.005 --double-id --biallelic-only"))
+bim <- fread(paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,".bim"),header=F)
+dup.id <- bim$V2[duplicated(bim$V2)]
+write.table(dup.id, file = paste0("/data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j),row.names = F,col.names = F,quote=F)
+system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j," --maf 0.005 --double-id --biallelic-only --exclude /data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j))
+
 # for(j in 1:22){
-#   bim <- fread(paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,".bim"),header=F)
-#   dup.id <- bim$V2[duplicated(bim$V2)]
-#   write.table(dup.id, file = paste0("/data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j),row.names = F,col.names = F,quote=F)
-#   
+# 
 # }
 
 #dup.id <- fread(paste0("/data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j),header=F)
-system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j," --maf 0.005 --double-id --biallelic-only --exclude /data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j))
+
 # for(j in 1:22){
 #   system(paste0("/data/zhangh24/software/plink2 --bfile /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j," --exclude /data/zhangh24/multi_ethnic/data/GHBS_plink/all_chr-merge.missnp --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr_clean",j))
 # }
