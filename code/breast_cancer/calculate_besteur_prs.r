39d3e3ee254e091d65390cc37656ee053d9bfa54
Zhang <andrew.haoyu@gmail.com>
2021-04-26 12:56:25 -0400

    update

--- code/breast_cancer/calculate_besteur_prs.r
+++ code/breast_cancer/calculate_besteur_prs.r
@@ -1,20 +1,35 @@
 #best eur prs
 setwd("/data/zhangh24/multi_ethnic/data/")
+#load GHBS_id
+bim <- fread("/data/zhangh24/multi_ethnic/data/GBHS_plink/all_chr_temp.bim",header=F) 
+bim = bim %>% 
+  unite("chr.pos",V1,V4,sep=":",remove=F)
+bim = bim %>% 
+  rename(GBHS_id = V2)
+
+
 trait = c("overall","erpos","erneg")
 library(data.table)
 library(tidyverse)
 
 prs.snp <- read.csv("/data/zhangh24/multi_ethnic/data/breast_cancer_330SNPs.csv",header=T)
 prs.snp = prs.snp %>% 
+  unite("chr.pos",CHR,Position,sep=":",remove=F)
+
+prs.snp = prs.snp %>% 
   select(variant,chr.pos,Reference.allele,Effect.allele)
 load(paste0("./AABC_data/BC_EUR_",trait[l],"_aligned.rdata"))
-prs.snp = prs.snp %>% 
-  unite("chr.pos",CHR,Position,sep=":",remove=F)
 prs.snp.eur = left_join(prs.snp,sum.data,by="chr.pos") %>% 
   filter(((Eff_allele==Effect.allele)&(Ref_allele==Reference.allele))|
-           (Eff_allele==Reference.allele)&(Ref_allele==Effect.allele))
+           (Eff_allele==Reference.allele)&(Ref_allele==Effect.allele)) %>% 
+  select(c(colnames(sum.data),"chr.pos","variant"))
 
-# jdx <- which(sum.data$chr.pos=="7:91459189")
-# sum.data[jdx,]
-idx <- which(prs.snp$variant%in%prs.snp.eur$variant==F)
-prs.snp[idx[1:10],]
+jdx <- which(bim$chr.pos=="1:114445880")
+
+
+
+prs.snp.eur.update = left_join(prs.snp.eur,bim,by="chr.pos") %>% 
+  filter(((Eff_allele==V5)&(Ref_allele==V6))|
+           (Eff_allele==V6)&(Ref_allele==V5))
+
+idx <- which(prs.snp.eur$ID%in%prs.snp.eur.update$ID==F)
--- code/breast_cancer/convert_vcf_to_plink.R
+++ code/breast_cancer/convert_vcf_to_plink.R
@@ -5,12 +5,12 @@ j = as.numeric(args[[1]])
 #remove all the multi-allelic snps
 library(data.table)
 library(dplyr)
-system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,"_temp --maf 0.005 --double-id"))
-bim <- fread(paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,"_temp.bim"),header=F)
+system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GBHS_plink/chr",j,"_temp --maf 0.001 --double-id"))
+bim <- fread(paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,".bim"),header=F)
 id = bim %>% mutate(id = paste0(V2,":",V6,":",V5)) %>% 
   select(id)
 bim$V2 = id
-write.table(bim,paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,"_temp.bim"),row.names = F,col.names = F,quote = F)
+write.table(bim,paste0("/gpfs/gsfs11/users/zhangh24/multi_ethnic/data/GHBS_plink/chr",j,".bim"),row.names = F,col.names = F,quote = F)
 # dup.id <- bim$V2[duplicated(bim$V2)]
 # write.table(dup.id, file = paste0("/data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j),row.names = F,col.names = F,quote=F)
 # system(paste0("/data/zhangh24/software/plink2 --vcf /gpfs/gsfs10/users/BC_risk_prediction/ghana/GSA/VCFs_to_share/subtracted_chr",j,".dose.vcf.gz --recode --make-bed --out /data/zhangh24/multi_ethnic/data/GHBS_plink/chr",j," --maf 0.005 --double-id --biallelic-only --exclude /data/zhangh24/multi_ethnic/data/GHBS_plink/dup.id.chr.",j))
