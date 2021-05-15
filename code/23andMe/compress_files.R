cc249c7a3eb8ec40ab731be60fa8f70c090a0e42
Zhang <andrew.haoyu@gmail.com>
2021-05-14 22:36:08 -0400

    compres

--- code/23andMe/compress_files.R
+++ code/23andMe/compress_files.R
@@ -4,4 +4,4 @@
 
 # system(paste0("cd /dcs04/nilanjan/data/23andme/cleaned; zip -r /dcs04/nilanjan/data/hzhang1/multi_ethnic/data/cleaned/",eth[i],".zip  ",eth[i]))
 #system(paste0("cd /data/zhangh24/multi_ethnic/data/cleaned; unzip ",eth[i],".zip"))
-system(paste0("cd /data/zhangh24/multi_ethnic/result/cleaned/organize_prs/; zip -r TDLD_EBall.zip TDLD_EBall"))
+system(paste0("cd /data/zhangh24/multi_ethnic/result/cleaned/organize_prs/; zip -r TDLD_EB.zip TDLD_EB"))
