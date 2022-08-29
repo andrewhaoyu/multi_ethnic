76cb125d54a19435e2933779a9bc462eb299995f
Zhang <andrew.haoyu@gmail.com>
2022-08-29 14:52:03 -0400

    glgc ct

--- code/GLGC_analysis/5_create_folder.R
+++ code/GLGC_analysis/5_create_folder.R
@@ -24,4 +24,18 @@ for(i in 5:5){
                   "mkdir ",trait[l]))
   }
 }
+system(paste0("cd /data/zhangh24/multi_ethnic/result/GLGC/; mkdir BestEUR"))
+for(i in 2:5){
+  system(paste0("cd /data/zhangh24/multi_ethnic/result/GLGC/BestEUR/; mkdir ",eth[i]))
+}
 
+system(paste0("cd /data/zhangh24/multi_ethnic/result/GLGC/prs/; mkdir BestEUR"))
+for(i in 2:5){
+  system(paste0("cd /data/zhangh24/multi_ethnic/result/GLGC/prs/BestEUR/; mkdir ",eth[i]))
+}
+# for(i in 5:5){
+#   for(l in 1:4){
+#     system(paste0("cd /data/zhangh24/multi_ethnic/result/GLGC/prs/PT/" ,eth[i],"/; ",
+#                   "mkdir ",trait[l]))
+#   }
+# }
--- code/GLGC_analysis/8_BestEUR.R
+++ code/GLGC_analysis/8_BestEUR.R
@@ -21,8 +21,8 @@ library(dplyr)
 
 eth_vec <- c("EUR","AFR","AMR","EAS","SAS")
 trait_vec <- c("HDL","LDL",
-           "logTG",
-           "TC")
+               "logTG",
+               "TC")
 eth = eth_vec[i]
 trait = trait_vec[l]
 sid<-Sys.getenv('SLURM_JOB_ID')
@@ -33,87 +33,53 @@ system(paste0("cp ",kg.dir,eth,"/all_chr.bed ",temp.dir,eth,"all_chr.bed"))
 system(paste0("cp ",kg.dir,eth,"/all_chr.bim ",temp.dir,eth,"all_chr.bim"))
 system(paste0("cp ",kg.dir,eth,"/all_chr.fam ",temp.dir,eth,"all_chr.fam"))
 
-# for(i in 1:2){
-#   for(l in 1:3){
+#load EUR results
 setwd("/data/zhangh24/multi_ethnic/")
 #temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[l],"/",eth[i],"/")
 data.dir = "/data/zhangh24/multi_ethnic/data/GLGC_cleaned/"
-out.dir = paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/",eth,"/",trait,"/")
-#########LD clumping#################
-#load gwas summary statistics
-sum.data = as.data.frame(fread(paste0(data.dir,eth,"/",trait,".txt"),header=T))
-# write.table(sum.data.MAF,file = paste0("/lscratch/",sid,"/test/",eth[i],"_summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1,".out")
-#             ,col.names = T,row.names = F,quote=F) 
-#prepare association file for plink
+#out.dir = paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/EUR/",trait,"/")
+
+load(paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/EUR/",trait,"/CT.result"))
+#find best index
+idx <- which.max(ct.result[[2]])
+pthres <- c(5E-08,5E-07,5E-06,5E-05,5E-04,5E-03,5E-02,5E-01,1.0)
+data.dir = "/data/zhangh24/multi_ethnic/data/GLGC_cleaned/"
+#########prepare summary statistics#################
+#load eur summary statistics
+sum.data = as.data.frame(fread(paste0(data.dir,"EUR/",trait,".txt"),header=T))
 sum.data.assoc = sum.data %>% 
-  mutate(P = as.numeric(P)) %>% 
   rename(SNP = rsID,
          BP = POS_b37) %>% 
   select(CHR,SNP,BP,A1,BETA,P) 
 
-#sum.data.assoc = sum.data.assoc[,c("CHR","SNP","BP","A1","BETA","P")]
-#idx <- which(sum.data.assoc$SNP=="rs4970836")
-write.table(sum.data.assoc,file = paste0(temp.dir,"all_chr_assoc.txt"),col.names = T,row.names = F,quote=F)
 
-# dim(summary)
-# head(summary)
-pthr = 1
-r2thr = 0.1
-kbpthr = 500#cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
-#code <- rep("c",5*3*3)
-#system(paste0("/data/zhangh24/software/plink2 --bfile /data/zhangh24/KG.plink/",eth[i],"/chr_all --clump ",cur.dir,eth[i],"/summary_out_MAF_rho_",l,"_size_",m,"_rep_",i_rep,".out --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",cur.dir,eth[i],"/LD_clump_rho_",l,"_size_",m,"_rep_",i_rep))
-res = system(paste0("/data/zhangh24/software/plink2 --bfile ",temp.dir,eth,"all_chr --clump ",temp.dir,"all_chr_assoc.txt --clump-p1 ",pthr," --clump-r2 ",r2thr,"  --clump-kb ",kbpthr," --out ",temp.dir,"LD_clump"))
-system(paste0("mv ",temp.dir,"LD_clump.clumped ",out.dir))
-if(res==2){
-  stop()
-}
-print("LD clumping end")
-#########LD clumping ended#################
+pthres <- c(5E-08,5E-07,5E-06,5E-05,5E-04,5E-03,5E-02,5E-01,1.0)
+LD <- as.data.frame(fread(paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/EUR/",trait,"/LD_clump.clumped")))
+clump.snp <- LD[,3,drop=F]
+#prepare the prs files with best EUR SNP and coefficients
+prs.all <- left_join(clump.snp,sum.data.assoc) %>% 
+  filter(P <= pthres[idx])
+n_pthres <- length(pthres)
+#q_range = data.frame(rep("p_value",n_pthres),rep(0,n_pthres),rep(0.5,n_pthres),stringsAsFactors = F)
+prs.file = prs.all[,c("SNP","A1","BETA")]
+write.table(prs.file,file = paste0(temp.dir,"prs_coeff"),col.names = T,row.names = F,quote=F)
 
 #########PRS calculation#################
+setwd("/data/zhangh24/multi_ethnic/")
+#temp.dir = paste0("/fastscratch/myscratch/hzhang1/ARIC/",trait[l],"/",eth[i],"/")
+out.dir = paste0("/data/zhangh24/multi_ethnic/result/GLGC/BestEUR/",eth,"/",trait,"/")
 temp.dir = paste0('/lscratch/',sid,'/test/')
 system(paste0("mkdir ",temp.dir,"ukb"))
 geno.data = paste0("/data/zhangh24/multi_ethnic/data/UKBB/genotype/all_data/")
 system(paste0("cp ",geno.data,eth,"/all_chr.bed ",temp.dir,"ukb/all_chr.bed"))
 system(paste0("cp ",geno.data,eth,"/all_chr.bim ",temp.dir,"ukb/all_chr.bim"))
 system(paste0("cp ",geno.data,eth,"/all_chr.fam ",temp.dir,"ukb/all_chr.fam"))
-
-
-pthres <- c(5E-08,5E-07,5E-06,5E-05,5E-04,5E-03,5E-02,5E-01,1.0)
-LD <- as.data.frame(fread(paste0(out.dir,"LD_clump.clumped")))
-clump.snp <- LD[,3,drop=F]
-prs.all <- left_join(clump.snp,sum.data.assoc)
-temp = 1
-out.dir = paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/",eth,"/",trait,"/")
-out.dir.prs <- paste0("/data/zhangh24/multi_ethnic/result/GLGC/prs/PT/",eth,"/",trait,"/")
-
-
-n_pthres <- length(pthres)
-#q_range = data.frame(rep("p_value",n_pthres),rep(0,n_pthres),rep(0.5,n_pthres),stringsAsFactors = F)
-
-prs.file = prs.all[,c("SNP","A1","BETA")]
-
-write.table(prs.file,file = paste0(temp.dir,"prs_coeff"),col.names = T,row.names = F,quote=F)
-p.value.file = prs.all[,c("SNP","P")]
-
-write.table(p.value.file,file = paste0(temp.dir,"p_value"),col.names = T,row.names = F,quote=F)
-
-q_range = data.frame(rep("p_value",n_pthres),rep(0,n_pthres),rep(0.5,n_pthres))
-
-temp = 1
-for(k in 1:length(pthres)){
-  q_range[temp,1] = paste0("p_value_",k)
-    q_range[temp,3] = pthres[k]
-    temp = temp + 1
-}
-q_range = q_range[1:(temp-1),]
-write.table(q_range,file = paste0(temp.dir,"q_range_file"),row.names = F,col.names = F,quote=F)
-
-
-res <- system(paste0("/data/zhangh24/software/plink2_alpha --q-score-range ",temp.dir,"q_range_file ",temp.dir,"p_value header --threads 2 --score ",temp.dir,"prs_coeff cols=+scoresums,-scoreavgs header no-mean-imputation  --bfile ",temp.dir,"ukb/all_chr --out ",temp.dir,"prs"))
+res <- system(paste0("/data/zhangh24/software/plink2_alpha ",
+                    "--threads 2 --score ",temp.dir,"prs_coeff cols=+scoresums,-scoreavgs header no-mean-imputation ",
+                    "--bfile ",temp.dir,"ukb/all_chr --out ",temp.dir,"prs"))
 system(paste0("ls ",temp.dir,""))
 if(res==2){
-    stop()
+  stop()
 }
 print("PRS calculation ended")
 
@@ -123,12 +89,12 @@ prs_list = list()
 temp = 1
 for(k in 1:length(pthres)){
   
-    prs_temp = fread(paste0(temp.dir,"prs.p_value_",k,".sscore"))
-    # times (2*number of SNPs)
-    prs_list[[temp]] = prs_temp[,5:ncol(prs_temp)]
-    
-    colnames(prs_list[[temp]]) = paste0("p_value_",k)
-    temp = temp + 1
+  prs_temp = fread(paste0(temp.dir,"prs.p_value_",k,".sscore"))
+  # times (2*number of SNPs)
+  prs_list[[temp]] = prs_temp[,5:ncol(prs_temp)]
+  
+  colnames(prs_list[[temp]]) = paste0("p_value_",k)
+  temp = temp + 1
   
 }
 prs_mat = as.data.frame(cbind(prs_temp[,1:2],bind_cols(prs_list)))
@@ -180,19 +146,20 @@ r2.result = data.frame(eth = eth,
 )
 #find best cutoff for EUR by using all data as tuning
 
-  pheno_tuning = pheno_all
-  r2_tun_vec = rep(0,length(pthres))
-  #calculate R2 for each of the tuning dataset
-  model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_tuning)
-  for(k in 1:length(pthres)){
-    prs = pheno_tuning[,paste0("p_value_",k)]
-    model.prs <- lm(model.null$residual~prs,data=pheno_tuning)
-    r2_tun_vec[k] = summary(model.prs)$r.square
-  }
-  
-  ct.result = list(r2.result,r2_tun_vec)
+pheno_tuning = pheno_all
+r2_tun_vec = rep(0,length(pthres))
+#calculate R2 for each of the tuning dataset
+model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_tuning)
+for(k in 1:length(pthres)){
+  prs = pheno_tuning[,paste0("p_value_",k)]
+  model.prs <- lm(model.null$residual~prs,data=pheno_tuning)
+  r2_tun_vec[k] = summary(model.prs)$r.square
+}
+
+ct.result = list(r2.result,r2_tun_vec)
 
 
 save(ct.result, file = paste0(out.dir, "CT.result"))
+load(paste0(out.dir, "CT.result"))
 #   }
 # }
