516211c9b4477146283d8cdfb113d95ee6fe2341
Zhang <andrew.haoyu@gmail.com>
2022-09-27 23:14:00 -0400

    ctsleb

--- code/AOU/CTSLEB/1_CTSLEB.R
+++ code/AOU/CTSLEB/1_CTSLEB.R
@@ -345,71 +345,169 @@ r2_ctsleb <- summary(model)$r.square
 
 
 save(r2_ctsleb, file = paste0(out.dir, "CTSLEB.result"))
+
+
+
+
+
+
+
+
+
+
+##########five ancestries analyses###############
+#########EB step###################
+eth_vec <- c("EUR","AFR","AMR")
+
+eth_other = setdiff(eth_vec, eth_vec[i])
+
+sum_other_list = list()
+for(i_eth in 1:length(eth_other)){
+  
+  
+  sum_temp = as.data.frame(fread(paste0(data.dir,eth_vec[i_eth],"/",trait,"_update.txt"),header=T))
+  sum_other_list[[i_eth]] = sum_temp %>% 
+    select(rsID, CHR, pos37, BETA, SE, A1, P) %>% 
+    rename(SNP = rsID, BP = pos37)
+  
+}
+other_ans_names = eth_other
+sum_com <- AlignSumMulti(sum_tar = sum_tar,
+                         sum_other_list = sum_other_list,
+                         other_ans_names = other_ans_names)
+
+unique_infor_post = EBpostMulti(unique_infor,SNP_set,
+                                sum_com,other_ans_names)
+
+eb_post_col_names = c("BETA_EB_target",paste0("BETA_EB_",other_ans_names[1]))
+post_beta_mat = unique_infor_post %>% 
+  select(all_of(eb_post_col_names))
+
+
+plink_file_eb = PreparePlinkFileEB(snp_list,
+                                   unique_infor_post,
+                                   post_beta_mat)
+score_file = plink_file_eb[[1]]
+write.table(score_file,file = paste0(temp.dir,"score_file_eb"),row.names = F,col.names = F,quote=F)
+#p_value_file description
+#the second column is the p-values of SNPs from the GWAS of the target population
+p_value_file = plink_file_eb[[2]]
+
+
+p_value_file_temp = p_value_file
+for(k1 in 1:length(pthres)){
+  #keep al the SNPs with P_EUR less than pthres[k1] in the analyses
+  idx <- which(unique_infor$P_other<=pthres[k1])
+  p_value_file_temp$P[idx] = 0
+  write.table(p_value_file_temp,file = paste0(temp.dir,"p_value_file"),col.names = F,row.names = F,quote=F)
+  n_col = ncol(score_file)
+  
+  res = system(paste0(soft.dir,"plink2_alpha ",
+                      "--q-score-range ",temp.dir,"q_range_file ",temp.dir,"p_value_file ",
+                      "--score-col-nums 3-",n_col," ",
+                      "--score ",temp.dir,"score_file_eb cols=+scoresums,-scoreavgs ",
+                      "--bfile ",temp.dir,"ukb/all_chr ",
+                      "--out ",temp.dir,"eb_prs_p_other_",k1))
+  #the output of plink2 create 9 different files named as prs_p_other_k1.p_tar_k2.sscore
+  #this output file contains 16 columns
+  #the column 1-4 are: family ID, individual ID, 2*total number of SNPs in the PRS, the sum of allele count
+  #column 5-16 are the PRS scores with SNP of p_target<p_thres[k2]|p_eur<p_thres[k1] for different combinations of r2-cutoff and base_window_size
+}
 #find best cutoff for EUR by using all data as tuning
 
 
-# #########R2 calculation################# 
-# # 
-# # 
-# pheno.dir = "/data/zhangh24/multi_ethnic/data/UKBB/phenotype/"
-# pheno = as.data.frame(fread(paste0(pheno.dir,trait,"/tuning+validation/",eth,"_all_data.txt")))
-# pheno = pheno[,1:2]
-# covar <- as.data.frame(fread(paste0(pheno.dir,"/covariates/tuning+validation/",eth,"_all_data.txt")))
-# pheno <- left_join(pheno, covar)
-# colnames(pheno) = c('id','y','sex','age',paste0('pc',1:10))
-# pheno_com = pheno[complete.cases(pheno$y),]
-# 
-# #combine prs with pheno_all
-# pheno_all = left_join(pheno_com,prs_mat,by = "id")
-# 
-# n_fold = 20
-# r2_vec = rep(0,n_fold)
-# for (fold in 1:n_fold){
-#   set.seed(123*fold)
-#   ids1 = sample(1:nrow(pheno_all), ceiling(nrow(pheno_all)/2), replace = F)
-#   ids2 = setdiff(1:nrow(pheno_all), ids1)
-#   
-#   pheno_tuning = pheno_all[ids1,]
-#   pheno_validation = pheno_all[ids2,]
-#   r2_tun_vec = rep(0,length(pthres))
-#   #calculate R2 for each of the tuning dataset
-#   model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_tuning)
-#   for(k in 1:length(pthres)){
-#     prs = pheno_tuning[,paste0("p_value_",k)]
-#     model.prs <- lm(model.null$residual~prs,data=pheno_tuning)
-#     r2_tun_vec[k] = summary(model.prs)$r.square
-#   }
-#   #find best idx
-#   idx = which.max(r2_tun_vec)
-#   #evaluate on validation
-#   model.vad.null  =  lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_validation)
-#   prs = pheno_validation[,paste0("p_value_",idx)]
-#   model.vad.prs <- lm(model.vad.null$residual~prs,data=pheno_validation)
-#   r2_vec[fold] = summary(model.vad.prs)$r.square
-#   
-# }
-# r2 = mean(r2_vec)
-# 
-# r2.result = data.frame(eth = eth,
-#                        trait = trait,
-#                        method = "CT",
-#                        r2 = r2
-# )
-# #find best cutoff for EUR by using all data as tuning
-# 
-# pheno_tuning = pheno_all
-# r2_tun_vec = rep(0,length(pthres))
-# #calculate R2 for each of the tuning dataset
-# model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_tuning)
-# for(k in 1:length(pthres)){
-#   prs = pheno_tuning[,paste0("p_value_",k)]
-#   model.prs <- lm(model.null$residual~prs,data=pheno_tuning)
-#   r2_tun_vec[k] = summary(model.prs)$r.square
-# }
-# 
-# ct.result = list(r2.result,r2_tun_vec)
-# 
-# 
-# save(ct.result, file = paste0(out.dir, "CT.result"))
-# #   }
-# # }
+prs_list = list()
+temp = 1
+#take the column name of different clumping parameters
+names = colnames(score_file)[3:ncol(score_file)]
+for(k1 in 1:length(pthres)){
+  for(k2 in 1:length(pthres)){
+    #the --score file cols=+scoresums,-scoreavgs command in plink2 computes PRS as G*beta
+    #If you compute PRS by chromosome, you need to sum the PRS scores for all chromosomes. 
+    #load PRS for SNPs with p_target<p_thres[k2]|p_eur<p_thres[k1] 
+    prs_temp = fread(paste0(temp.dir,"eb_prs_p_other_",k1,".p_tar_",k2,".sscore"))
+    # times (2*number of SNPs)
+    prs_list[[temp]] = prs_temp[,5:ncol(prs_temp)]
+    
+    colnames(prs_list[[temp]]) = paste0(names,"_","p_other_",pthres[k1],"_p_tar_",pthres[k2])
+    temp = temp + 1
+  }
+}
+prs_mat = as.data.frame(cbind(prs_temp[,1:2],bind_cols(prs_list)))
+colnames(prs_mat)[2] = "id"
+prs_score = prs_mat[,-c(1:2)]
+
+#############EB step finish############################
+
+
+
+
+
+
+
+############SL step start#############################
+pheno.dir = "/data/zhangh24/multi_ethnic/data/UKBB/phenotype/"
+pheno_tuning = as.data.frame(fread(paste0(pheno.dir,trait,"/tuning+validation/",eth,"_tuning.txt")))
+pheno_tuning = pheno_tuning[,1:2]
+covar <- as.data.frame(fread(paste0(pheno.dir,"/covariates/tuning+validation/",eth,"_all_data.txt")))
+pheno_tuning <- left_join(pheno_tuning, covar)
+colnames(pheno_tuning) = c('id','y','sex','age',paste0('pc',1:10))
+pheno_tuning_com = pheno_tuning[complete.cases(pheno_tuning$y),]
+pheno_tuning = left_join(pheno_tuning_com,prs_mat,by = "id")
+model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_tuning)
+y_tun = model.null$residual
+prs_tun = pheno_tuning[,colnames(prs_score)]
+pheno_vad = as.data.frame(fread(paste0(pheno.dir,trait,"/tuning+validation/",eth,"_validation.txt")))
+pheno_vad = pheno_vad[,1:2]
+pheno_vad <- left_join(pheno_vad, covar)
+colnames(pheno_vad) = c('id','y','sex','age',paste0('pc',1:10))
+pheno_vad_com = pheno_vad[complete.cases(pheno_vad$y),]
+pheno_vad = left_join(pheno_vad_com,prs_mat,by = "id")
+model.null <- lm(y~pc1+pc2+pc3+pc4+pc5+pc6+pc7+pc8+pc9+pc10+age+sex,data=pheno_vad)
+y_vad = model.null$residual
+prs_vad = pheno_vad[,colnames(prs_score)]
+library(caret)
+
+mtx = cor(prs_tun)
+drop = findCorrelation(mtx,cutoff=0.98)
+drop = names(prs_tun)[drop]
+
+prs_tun_clean = prs_tun %>% 
+  select(-all_of(drop))
+prs_vad_clean = prs_vad %>% 
+  select(-all_of(drop))
+library(ranger)
+library(SuperLearner)
+#choose the prediction algorithms
+SL.libray <- c(
+  "SL.glmnet",
+  "SL.ridge",
+  "SL.nnet"
+  #"SL.bayesglm"
+  #"SL.stepAIC"
+  #"SL.xgboost"
+  #"SL.randomForest"
+  #"SL.ksvm",
+  #"SL.bartMachine", 
+  #"SL.kernelKnn",
+  #"SL.rpartPrune", 
+  #"SL.lm"
+  #"SL.mean"
+)
+sl = SuperLearner(Y = y_tun, X = prs_tun_clean, family = gaussian(),
+                  # For a real analysis we would use V = 10.
+                  # V = 3,
+                  SL.library = SL.libray)
+
+y_pred <- predict(sl, prs_vad_clean, onlySL = TRUE)
+#evaluate the CT-SLEB prs performance on the validation
+model <- lm(y_vad~y_pred[[1]])
+r2_ctsleb <- summary(model)$r.square
+
+
+
+
+
+save(r2_ctsleb, file = paste0(out.dir, "CTSLEB_all.result"))
+
+
---
+++ code/AOU/Polypred/4_merge_polypred_result.R
@@ -0,0 +1,19 @@
+eth_vec <- c("EUR","AFR","AMR")
+trait_vec <-c("height","bmi")
+library(data.table)
+result_list = list()
+temp = 1
+for(l in 1:2){
+  for(i in 2:3){
+    eth = eth_vec[i]
+    trait = trait_vec[l]
+    out.dir = paste0("/data/zhangh24/multi_ethnic/result/AOU/Polypred/",eth_vec[i],"/",trait_vec[l],"/")
+    load(paste0(out.dir, "prscsx.result"))
+    
+    result_list[[temp]] = r2.result
+    temp = temp+ 1
+  }
+}
+
+final_result = rbindlist(result_list)
+save(final_result, file = "/data/zhangh24/multi_ethnic/result/AOU/XPASS/xpass.rdata")
