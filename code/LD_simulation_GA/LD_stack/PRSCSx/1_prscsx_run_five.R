66db7bf04bab55c86f7feb6220c35b72a96d8f87
Zhang <andrew.haoyu@gmail.com>
2022-09-28 23:39:44 -0400

    prxcsx

--- code/LD_simulation_GA/LD_stack/PRSCSx/1_prscsx_run_five.R
+++ code/LD_simulation_GA/LD_stack/PRSCSx/1_prscsx_run_five.R
@@ -4,6 +4,7 @@ l = as.numeric(args[[1]])
 m = as.numeric(args[[2]])
 i_rep = as.numeric(args[[3]])
 i1 = as.numeric(args[[4]])
+k = as.numeric(args[[5]])
 library(data.table)
 library(tidyverse)
 eth <- c("EUR","AFR","AMR","EAS","SAS")
@@ -21,7 +22,7 @@ system(paste0("cp /data/zhangh24/software/PRScsx/1KGLD/snpinfo_mult_1kg_hm3 ",te
 cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_new/"
 out.dir.sum <-  "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
 out.dir <-  "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/LD_stack/"
-system(paste0("cp ",out.dir, "all_eth.bim", temp.dir, "all_eth.bim"))
+system(paste0("cp ",out.dir, "all_eth.bim ", temp.dir,"all_eth.bim"))
 path_to_bim = paste0(temp.dir,"all_eth")
 setwd("/data/zhangh24/multi_ethnic/")
 load("./result/LD_simulation_new/snp.infor.match37_38.rdata")
@@ -48,39 +49,10 @@ for(i in 1:5){
 }
 
 
-
-summary.eur <- as.data.frame(fread(paste0(out.dir.sum,eth[1],"/summary_out_rho_",l,"_size_",4,"_rep_",i_rep,"_GA_",i1)))  
-#prepare EUR summary stat for a specific chr
-#load 1kg snp list
-summary.eur = inner_join(summary.eur,prs_cs_ref,by = c("rs_id"="SNP"))
-summary.eur = summary.eur %>% 
-  separate(SNP,into=c("non_im","non_im2","first_allele","second_allele"),
-           remove=F) %>% 
-  mutate(A2 =ifelse(A1==second_allele,first_allele,second_allele)) %>% 
-  select(rs_id,A1,A2,BETA,P) %>% 
-  rename(SNP=rs_id)
-write.table(summary.eur,file = paste0(temp.dir,"EUR_sumstats.txt"),row.names = F,col.names = T,quote=F)
-#merge with prs-csx reference data
-#prepare target summary stat for a specific chr
-summary.tar <- as.data.frame(fread(paste0(out.dir.sum,eth[i],"/summary_out_rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1)))  
-summary.tar = left_join(summary.tar,snp.infor,by="SNP")
-#merge with prs-csx reference data
-summary.tar = inner_join(summary.tar,prs_cs_ref,by = c("rs_id"="SNP"))
-summary.tar.sub = summary.tar %>% 
-  separate(SNP,into=c("non_im","non_im2","first_allele","second_allele"),
-           remove=F) %>% 
-  mutate(A2 =ifelse(A1==second_allele,first_allele,second_allele)) %>% 
-  select(rs_id,A1,A2,BETA,P) %>% 
-  rename(SNP=rs_id)
-write.table(summary.tar.sub,file = paste0(temp.dir,eth[i],"_sumstats.txt"),row.names = F,col.names = T,quote=F)
-bim = read.table(paste0(cur.dir,eth[i],"/all_chr_test.mega.bim"))
-bim.update =inner_join(summary.tar,bim,by = c("SNP"="V2")) %>% 
-  select(V1,rs_id,V3,V4,V5,V6)
-write.table(bim.update,file = paste0(temp.dir,"all_chr_test.mega.bim"),row.names = F,col.names = F,quote=F)
 #phi = c(1E-6,1E-4)
 #phi = c(1E-02)
 phi = c(1,1E-02,1E-04,1E-06)
-for(k in 1:length(phi)){
+#for(k in 1:length(phi)){
   for(j in 1:22){
     print(j)
     #create bim file for prscsx
@@ -93,18 +65,18 @@ for(k in 1:length(phi)){
                 python /data/zhangh24/software/PRScsx/PRScsx.py", 
                   " --ref_dir=",path_to_ref,
                   " --bim_prefix=",path_to_bim,
-                  " --sst_file=",path_to_sum,"EUR_sumstats.txt,",path_to_sum,eth[i],"_sumstats.txt",
-                  " --n_gwas=100000,",size_list[m],
-                  " --pop=EUR,",eth[i],
+                  " --sst_file=",path_to_sum,"EUR_sumstats.txt,",path_to_sum,"AFR_sumstats.txt,",path_to_sum,"AMR_sumstats.txt,",path_to_sum,"EAS_sumstats.txt,",path_to_sum,"SAS_sumstats.txt",
+                  " --n_gwas=100000,",size_list[m],",",size_list[m],",",size_list[m],",",size_list[m],
+                  " --pop=EUR,AFR,AMR,EAS,SAS",
                   " --chrom=",j,
                   " --phi=",phi[k], 
-                  " --out_dir=",out.dir.sum,eth[i],"/prscsx",
+                  " --out_dir=",out.dir.sum,eth[1],"/prscsx",
                   " --out_name=rho_",l,"_size_",m,"_rep_",i_rep,"_GA_",i1))
     
     
     
   }
   
-}
+#}
 
 
