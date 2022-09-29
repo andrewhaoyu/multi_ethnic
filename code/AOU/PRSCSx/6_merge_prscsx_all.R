48f54745e2240057027f2eb30219bcaa601dd5f4
Zhang <andrew.haoyu@gmail.com>
2022-09-28 22:12:42 -0400

    update

---
+++ code/AOU/PRSCSx/6_merge_prscsx_all.R
@@ -0,0 +1,28 @@
+
+library(data.table)
+#install.packages("dplyr")
+#install.packages("vctrs")
+library(dplyr)
+
+eth <- c("EUR","AFR","AMR")
+trait_vec <-c("height","bmi")
+
+
+  for(l in 1:2){
+    trait = trait_vec[l]
+    out.dir.prs = paste0("/data/zhangh24/multi_ethnic/result/AOU/prs/PRSCSX/EUR/",trait,"/")
+    phi = c("1e+00","1e-02","1e-04","1e-06")
+    for(i in 1:3){
+    for(v in 1:4){
+      data.list = list()
+      for(j in 1:22){
+        data = fread(paste0(out.dir.prs,"sum_",eth[i],"_pst_eff_a1_b0.5_phi",phi[v],"_chr",j,".txt"))
+        data.list[[j]] = data
+      }  
+      data = rbindlist(data.list)
+      save(data,file = paste0(out.dir.prs,"sum_",eth[i],"_pst_eff_a1_b0.5_phi",phi[v],".rdata"))
+      
+    }
+    
+  }
+}
