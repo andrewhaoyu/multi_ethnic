808490c35f7afff8922b6f021027837090c9d01f
Zhang <andrew.haoyu@gmail.com>
2023-01-12 17:29:58 -0500

    update

--- code/GLGC_analysis/CTSLEB/2_merge_CTSLEB.R
+++ code/GLGC_analysis/CTSLEB/2_merge_CTSLEB.R
@@ -11,13 +11,13 @@ for(l in 1:4){
     trait = trait_vec[l]
     out.dir = paste0("/data/zhangh24/multi_ethnic/result/GLGC/clumping_result/PT/",eth,"/",trait,"/")
     load(paste0(out.dir, "CTSLEB.result"))
-    r2.result = data.frame(eth = eth_vec[i],
-                           trait = trait_vec[l],
-                           method = "CT-SLEB",
-                           r2 = r2_ctsleb
-    )
+    # r2.result = data.frame(eth = eth_vec[i],
+    #                        trait = trait_vec[l],
+    #                        method = "CT-SLEB",
+    #                        r2 = r2_ctsleb
+    # )
     
-    result_list[[temp]] = r2.result
+    result_list[[temp]] = result
     temp = temp+ 1
   }
 }
