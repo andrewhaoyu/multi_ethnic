f7649202315dc0d52aaeba4faa613867bc731e01
Zhang <andrew.haoyu@gmail.com>
2023-01-14 13:55:58 -0500

    prscsx

--- code/AOU/PRSCSx/3_compute_prs_r2.R
+++ code/AOU/PRSCSx/3_compute_prs_r2.R
@@ -119,10 +119,27 @@ l = as.numeric(args[[2]])
     
     model = lm(y_vad~ prs_vad)
     r2 = summary(model)$r.square
+    
+    data = data.frame(y = y_vad, x = prs_vad)
+    R2Boot = function(data,indices){
+      boot_data = data[indices, ]
+      model = lm(y ~ x, data = boot_data)
+      result = summary(model)$r.square
+      return(c(result))
+    }
+    library(boot)
+    boot_r2 = boot(data = data, statistic = R2Boot, R = 10000)
+    
+    ci_result = boot.ci(boot_r2, type = "bca")
+    
+    
+    
     r2.result = data.frame(eth = eth[i],
                            trait = trait_vec[l],
                            method = "PRS-CSx",
-                           r2 = r2
+                           r2 = r2,
+                           r2_low = ci_result$bca[4],
+                           r2_high = ci_result$bca[5]
     )
     out.dir = paste0("/data/zhangh24/multi_ethnic/result/AOU/PRSCSX/",eth[i],"/",trait,"/")
     save(r2.result, file = paste0(out.dir, "prscsx.result"))
