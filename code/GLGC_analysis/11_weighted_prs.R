54488fb96bef01b3e5ce559a7c4dc3aab5672d46
Haoyu Zhang <andrew.haoyu@gmail.com>
2023-11-14 09:39:28 -0500

    update

--- code/GLGC_analysis/11_weighted_prs.R
+++ code/GLGC_analysis/11_weighted_prs.R
@@ -168,7 +168,3 @@ result = data.frame(eth = eth,
 
 save(result, file = paste0(out.dir, "weighted_prs_ct.result"))
 
-
-out_dir_boot = "/data/zhangh24/multi_ethnic/result/GLGC/boot_result/weighted_prs/"
-boot_result = list(boot_r2,ci_result)
-save(boot_result, paste0(out_dir_boot, "boot_result.rdata"))
