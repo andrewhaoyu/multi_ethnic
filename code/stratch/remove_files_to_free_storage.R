ca73db7fc0e06915cf9f0771d092f377311b1b9f
Haoyu Zhang <andrew.haoyu@gmail.com>
2024-09-11 17:53:48 -0400

    update

--- code/stratch/remove_files_to_free_storage.R
+++ code/stratch/remove_files_to_free_storage.R
@@ -2,6 +2,7 @@
 Args = commandArgs(trailingOnly = T)
 i = as.numeric(Args[[1]])
 
+
 eth = c("EUR","AMR","AFR",
         "EAS","SAS")
 system(paste0("cd /data/zhangh24/multi_ethnic/result/LD_simulation_GA/LD_stack/;cd ",eth[i],";rm -rf prs"))
