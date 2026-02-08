4f9411ab0503a7452d8286de1d879b3c2ade0061
Haoyu Zhang <andrew.haoyu@gmail.com>
2026-02-08 11:32:22 -0500

    update

--- code/scratch/prepare_full_summary_statistics_to_share.r
+++ code/scratch/prepare_full_summary_statistics_to_share.r
@@ -1,17 +1,21 @@
-cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
-tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/summary_data/"
+# cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
+# tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/summary_data/"
+# 
+# 
+# #five ethnics i
+# eth <- c("EUR","AFR","AMR","EAS","SAS")
+# for(i in 1:5){
+#   temp.code <- paste0("cp ",cur.dir,eth[i],"/summary_out_rho_* ",tar.dir,"",eth[i],"/")
+#   system(temp.code)
+# }
+# 
+# tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/cau_snp_effect_size/"
+# eth <- c("EUR","AFR","AMR","EAS","SAS")
+# for(i in 1:5){
+#   temp.code <- paste0("cp ",cur.dir,eth[i],"/select.cau_rho* ",tar.dir,"",eth[i],"/")
+#   system(temp.code)
+# }
 
 
-#five ethnics i
-eth <- c("EUR","AFR","AMR","EAS","SAS")
-for(i in 1:5){
-  temp.code <- paste0("cp ",cur.dir,eth[i],"/summary_out_rho_* ",tar.dir,"",eth[i],"/")
-  system(temp.code)
-}
-
-tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/cau_snp_effect_size/"
-eth <- c("EUR","AFR","AMR","EAS","SAS")
-for(i in 1:5){
-  temp.code <- paste0("cp ",cur.dir,eth[i],"/select.cau_rho* ",tar.dir,"",eth[i],"/")
-  system(temp.code)
-}
+temp.code <- "cd /data/DCEG_shared/datashare; tar -czvf simulated_data_meta.tar.gz simulated_data_meta/"
+system(temp.code)
