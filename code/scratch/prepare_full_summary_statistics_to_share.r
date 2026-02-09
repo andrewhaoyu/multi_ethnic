a2f13a78a5a1e92693cfcf5937acffa5ad90d1ad
Haoyu Zhang <andrew.haoyu@gmail.com>
2026-02-08 23:28:43 -0500

    update

--- code/scratch/prepare_full_summary_statistics_to_share.r
+++ code/scratch/prepare_full_summary_statistics_to_share.r
@@ -1,14 +1,14 @@
-# cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
-# tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/summary_data/"
-# 
-# 
-# #five ethnics i
-# eth <- c("EUR","AFR","AMR","EAS","SAS")
-# for(i in 1:5){
-#   temp.code <- paste0("cp ",cur.dir,eth[i],"/summary_out_rho_* ",tar.dir,"",eth[i],"/")
-#   system(temp.code)
-# }
-# 
+cur.dir <- "/data/zhangh24/multi_ethnic/result/LD_simulation_GA/"
+tar.dir <- "/data/BB_Bioinformatics/HZ/simulated_data_meta/"
+
+
+#five ethnics i
+eth <- c("EUR","AFR","AMR","EAS","SAS")
+for(i in 1:5){
+  temp.code <- paste0("cp ",cur.dir,eth[i],"/summary_out_rho_* ",tar.dir,"",eth[i],"/")
+  system(temp.code)
+}
+
 # tar.dir <- "/data/DCEG_shared/datashare/simulated_data_meta/cau_snp_effect_size/"
 # eth <- c("EUR","AFR","AMR","EAS","SAS")
 # for(i in 1:5){
@@ -16,15 +16,11 @@
 #   system(temp.code)
 # }
 
+temp.code <- "cd /data/BB_Bioinformatics/HZ/simulated_data_meta/; tar -czvf simulated_data_meta.tar.gz simulated_data_meta/"
+system(temp.code)
 
 # temp.code <- "cd /data/DCEG_shared/datashare; tar -czvf simulated_data_meta.tar.gz simulated_data_meta/"
 # system(temp.code)
 
-temp.code <- "rsync -avnc \
-  /data/DCEG_shared/datashare/simulated_data_meta/ \
-  /data/BB_Bioinformatics/HZ/simulated_data_meta/"
 
-system(temp.code)
 
-temp.code <- "cd /data/BB_Bioinformatics/HZ/simulated_data_meta/; tar -czvf simulated_data_meta.tar.gz simulated_data_meta/"
-system(temp.code)
